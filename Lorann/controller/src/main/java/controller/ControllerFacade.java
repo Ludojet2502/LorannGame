dd6a78cf5845c35ec64391f604cf56b3a002393b
rubisetcie <rubisetcie@10.192.131.199>
2018-05-28 15:52:30 +0200

    Maj Model

--- Lorann/controller/src/main/java/controller/ControllerFacade.java
+++ Lorann/controller/src/main/java/controller/ControllerFacade.java
@@ -44,7 +44,7 @@ public class ControllerFacade implements IController {
     public void start() throws SQLException {
         this.getView().displayMessage(this.getModel().getElementByPos(1, 1).toString());
 
-        this.getView().displayMessage(this.getModel().getElementByType(' ').toString());
+        this.getView().displayMessage(this.getModel().getElementByType('g').toString());
 
         final List<Element> Elements = this.getModel().getAllElements();
         final StringBuilder message = new StringBuilder();
