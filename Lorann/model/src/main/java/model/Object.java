fbf064e79735a884c59177f4526f3ca8d7786dd3
HugoLeBoennec <hugo.leboennec@viacesi.fr>
2018-05-28 16:38:25 +0200

    .

--- Lorann/model/src/main/java/model/Object.java
+++ Lorann/model/src/main/java/model/Object.java
@@ -2,6 +2,24 @@ package model;
 /**
  * <h1>The Constructor Object</h1>
  */
+
 public class Object {
+	int x;
+	int y;
+	
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	
 
 }
