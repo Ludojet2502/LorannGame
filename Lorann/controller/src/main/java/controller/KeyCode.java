0f67f85a177fd3d54676ead8ee09fa053c0550b2
rubisetcie <rubisetcie@10.192.131.199>
2018-05-29 09:07:51 +0200

    MAJ keycode

--- Lorann/controller/src/main/java/controller/KeyCode.java
+++ Lorann/controller/src/main/java/controller/KeyCode.java
@@ -3,8 +3,20 @@ package controller;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
+/**
+ * <h1>The Class KeyCode represents the keyboard code of a key pressed.</h1>
+ *
+ * @author Julien LIGUORI julien.liguori@viacesi.fr
+ * @version 1.0
+ */
 public class KeyCode extends KeyAdapter {
+	
+	/** The code of the last key pressed. */
 	static int key = 0;
+	
+	/**
+     * Called when a key is pressed
+     */
 	public void keyPressed(KeyEvent event) {
 		key = event.getKeyCode();
 	}
